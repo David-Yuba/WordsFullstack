c9682012083ef36aca0c15c9e7b77cb29184ffce
David-Yuba <dado2012yuba@gmail.com>
2025-12-14 22:07:14 +0100

    Refactored routes a base Route class which is extended

--- words.client/src/app/routes/blogs/BlogsRoute.component.ts
+++ words.client/src/app/routes/blogs/BlogsRoute.component.ts
@@ -1,29 +1,21 @@
-import { Component, inject, computed, ElementRef, afterEveryRender, Renderer2, ChangeDetectorRef, signal, WritableSignal } from "@angular/core";
+import { Component, computed, ElementRef, Renderer2, ChangeDetectorRef, signal, WritableSignal } from "@angular/core";
 
 
 import SearchSubMenu from "../../components/searchSubMenu/searchsubmenu.component";
 import Cards from "../../components/cards/cards.component";
 import Scrollbar from "../../components/scrollbar/scrollbar.component";
-import { ScrollbarModelData } from "../../components/scrollbar/scrollbar.service";
+import RouteComponent from "../route/route.component";
 
-import BlogData, { Blog } from "../../services/blogs.service";
+import { Blog } from "../../services/blogs.service";
 import { BlogInfo } from "../../services/blogs.service";
 
 @Component({
   selector: "blogs-route",
   imports: [SearchSubMenu, Cards, Scrollbar],
   templateUrl: "BlogsRoute.component.html",
-  styleUrl: "BlogsRoute.component.css",
-  host: {
-    class: "route",
-    "(wheel)": "this.scrollbarModel.updateScrollPercentage($event)",
-    "(pointerdown)": "onPointerDown($event)",
-    "(pointerup)": "onPointerUp($event)",
-    "(pointerleave)": "onPointerLeave($event)",
-  }
+  styleUrl: "BlogsRoute.component.css"
 })
-export default class BlogsRoute {
-  data = inject(BlogData);
+export default class BlogsRoute extends RouteComponent{
   blogInfo: WritableSignal<Array<BlogInfo>> = signal([]);
 
   filteredBlogs = signal<Array<number>>([]);
@@ -39,46 +31,14 @@ export default class BlogsRoute {
       }
     )
   });
-
-  scrollbarModel = inject(ScrollbarModelData);
-  scrollPosition = computed(() => {
-    return -this.scrollbarModel.scrollPercentage()*(this.scrollbarModel.elementHeight()-(this.scrollbarModel.getWindowHeight()-this.scrollbarModel.getHeaderHeight()))
-  });
-
-  constructor (private ref: ChangeDetectorRef, private el: ElementRef, private renderer: Renderer2){
-    this.scrollbarModel.setElementRef(inject(ElementRef));
+ 
+  constructor(private ref: ChangeDetectorRef, el: ElementRef, renderer: Renderer2) {
+    super(el, renderer);
     this.data.getBlogs().then(
       blogs => {
         const result = blogs.map((blog: Blog) => ({ blog_ID: blog.blog_ID, title: blog.title, img: blog.img, date_created: blog.date_created, written_by: blog.written_by, tags: blog.tags, short_description: blog.short_description }));
         this.data.emptyBlog = result.shift();
         this.blogInfo.set(result);
       })
-
-    afterEveryRender(() =>{
-      this.scrollbarModel.windowWidth.set(this.scrollbarModel.getWindowWidth());
-      this.scrollbarModel.windowHeight.set(this.scrollbarModel.getWindowHeight());
-      this.scrollbarModel.elementHeight.set(this.scrollbarModel.getElementHeight());
-    })
-  }
-
-  private pointerDownCleanUp?: () => void;
-  onPointerDown(e: PointerEvent) {
-    this.pointerDownCleanUp = this.renderer.listen(
-      this.el.nativeElement,
-      "pointermove",
-      (pointerEvent) => {
-        const startingPoint = e.clientY;
-        const deltaPosition = startingPoint - pointerEvent.clientY;
-        this.scrollbarModel.setScrollPercentage(deltaPosition/10);
-      }
-    )
-  }
-  onPointerUp(e: PointerEvent) {
-    if(this.pointerDownCleanUp)
-      this.pointerDownCleanUp();
-  }
-  onPointerLeave(e: PointerEvent) {
-    if(this.pointerDownCleanUp)
-      this.pointerDownCleanUp();
   }
 }
--- words.client/src/app/routes/home/HomeRoute.component.ts
+++ words.client/src/app/routes/home/HomeRoute.component.ts
@@ -1,95 +1,27 @@
-import { Component, inject, ElementRef, afterEveryRender, computed, Renderer2, signal, WritableSignal } from "@angular/core";
+import { Component, ElementRef, Renderer2, signal, WritableSignal } from "@angular/core";
 
 import NavSubMenu from "../../components/navSubMenu/navsubmenu.component";
 import DesktopNavSubMenu from "../../components/desktopNavSubMenu/navsubmenu.component";
 import Blog from "../../components/blog/blog.component";
 import Scrollbar from "../../components/scrollbar/scrollbar.component";
-import { ScrollbarModelData } from "../../components/scrollbar/scrollbar.service";
+import RouteComponent from "../route/route.component";
 
-import BlogData, { BlogSection } from "../../services/blogs.service";
+import { BlogSection } from "../../services/blogs.service";
 
 @Component({
   selector: "home-route",
   imports: [ NavSubMenu, DesktopNavSubMenu, Blog, Scrollbar ],
   templateUrl: "HomeRoute.component.html",
   styleUrl: "HomeRoute.component.css",
-  host: {
-    class: "route",
-    "(wheel)": "this.scrollbarModel.updateScrollPercentage($event)",
-    "(click)": "onHeadingClick($event)",
-    "(pointerdown)": "onPointerDown($event)",
-    "(pointerup)": "onPointerUp($event)",
-    "(pointerleave)": "onPointerLeave($event)",
-  }
 })
-export default class HomeRoute {
-  data = inject(BlogData);
+export default class HomeRoute extends RouteComponent {
   blogContentObject: WritableSignal<Array<BlogSection>> = signal([]);
 
-  scrollbarModel = inject(ScrollbarModelData);
-  scrollPosition = computed(() => {
-    return -this.scrollbarModel.scrollPercentage()*(this.scrollbarModel.elementHeight()-(this.scrollbarModel.getWindowHeight()-this.scrollbarModel.getHeaderHeight()))
-  });
-  headerPositions?: Array<number>;
-
-  constructor(private el: ElementRef, private renderer: Renderer2) {
-    this.scrollbarModel.setElementRef(inject(ElementRef));
+  constructor(el: ElementRef, renderer: Renderer2) {
+    super(el, renderer);
 
     this.data.getBlogById(0).then((blog) => {
       this.blogContentObject.set(this.data._getBlogObjectContent(blog));
     });
-
-    afterEveryRender(() =>{
-      this.scrollbarModel.windowWidth.set(this.scrollbarModel.getWindowWidth());
-      this.scrollbarModel.windowHeight.set(this.scrollbarModel.getWindowHeight());
-      this.scrollbarModel.elementHeight.set(this.scrollbarModel.getElementHeight());
-      this.headerPositions = this.setHeaderPositions();
-    })
-  }
-
-
-  setHeaderPositions() {
-    const headers = Array.from(window.document.querySelectorAll(".blog>*"));
-
-    return headers.map(header => header.getBoundingClientRect().top);
-  }
-
-  onHeadingClick(event: MouseEvent){
-    event.stopPropagation()
-    let index = Number((event.target as HTMLElement).dataset["pos"]);
-    if(index && this.headerPositions){
-      const padding = 16;
-      const headerHeight = this.scrollbarModel.getHeaderHeight();
-      const offset = (headerHeight === 0) ? padding : headerHeight;
-      const position = this.headerPositions[index] - offset;
-      this.scrollbarModel.setScrollPercentage(position);
-
-      const targetHeading = window.document.querySelectorAll("blog>*")[index];
-      this.renderer.setStyle(targetHeading, "color", "var(--highlighted-heading)");
-      setTimeout(()=> {
-        this.renderer.setStyle(targetHeading, "color", "var(--font-color)");
-      }, 1000)
-    }
-  }
-
-  private pointerDownCleanUp?: () => void;
-  onPointerDown(e: PointerEvent) {
-    this.pointerDownCleanUp = this.renderer.listen(
-      this.el.nativeElement,
-      "pointermove",
-      (pointerEvent) => {
-        const startingPoint = e.clientY;
-        const deltaPosition = startingPoint - pointerEvent.clientY;
-        this.scrollbarModel.setScrollPercentage(deltaPosition/10);
-      }
-    )
-  }
-  onPointerUp(e: PointerEvent) {
-    if(this.pointerDownCleanUp)
-      this.pointerDownCleanUp();
-  }
-  onPointerLeave(e: PointerEvent) {
-    if(this.pointerDownCleanUp)
-      this.pointerDownCleanUp();
   }
 }
